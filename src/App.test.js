39841f400b763e7cf1cadc3ec456dc1659fe8200
daniel dantas <Daniel Dantas>
2024-01-17 16:12:31 -0300

    Add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import '@testing-library/jest-dom/extend-expect';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { Article } from './components/Articles/Article';
+
+test('Exibe o título e o conteúdo do artigo', () => {
+  render(<Article tumbnail="./assets/images/artigo1.png" title="Exemplo de teste 1" provider="Nasa em teste 1" description="Lorem ipsum dolor ..." />);
+  const imgElement=screen.getByAltText(/Exemplo de teste 1/i);
+  const titleElement = screen.getByText(/Exemplo de teste 1/i);
+  const providerElement = screen.getByText(/Nasa em teste 1/i);
+  const descriptionElement = screen.getByText(/Lorem ipsum dolor .../i);
+  expect(imgElement).toBeInTheDocument();
+  expect(titleElement).toBeInTheDocument();
+  expect(providerElement).toBeInTheDocument();
+  expect(descriptionElement).toBeInTheDocument();
 });
