234b8d382393169eeccb80ff2b16925129433c1e
daniel dantas <Daniel Dantas>
2024-01-27 17:36:12 -0300

    Add YARN, call API with axios and Hooks useEffect/useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,49 @@
 import './styles/App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import {Navbar} from './components/Navbar/Navbar';
 import { Article } from './components/Articles/Article';
-import imgArticle from './assets/images/artigo1.png';
-import imgArticle2 from './assets/images/artigo2.png';
-import imgArticle3 from './assets/images/artigo3.png';
 
-class App extends React.Component {
+interface NewsItem{
+  id:string;
+  title:string;
+  newsSite:string;
+  summary:string;
+  imageUrl:string;
+}
 
-  render (){
-    return(
-      <> 
-      <Navbar/>
-      <section id='articles'>
-        <Article 
-        title='Designing Dashboards' 
-        provider='Nasa' 
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        thumbnail={imgArticle}
-        />
-
-        <Article title='Vibrant Portraits of 2024' 
-        provider='SpaceNews' 
-        description="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        thumbnail={imgArticle2}
-        />
-        
-        <Article title='36 Days of Malayalam type' 
-        provider='SpaceFlight Now' 
-        description="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
-        thumbnail={imgArticle3}
-        />
-      
-      </section>
-      </>
-    );
-  }
+export const App: React.FC = () => {
+  const [news, setNews] = useState<NewsItem[]>([]);
+  useEffect(()=>{
+    async function loadNews() {
+      try{
+        const response = await axios.get<NewsItem[]>('https://api.spaceflightnewsapi.net/v3/articles');
+        const newsData=response.data;
+        console.log(newsData);
+        setNews(newsData);
+      }catch(error){
+        console.error('Error fetching news:', error);
+      }
+    }
+    loadNews();
+  }, []);
+  return(
+    <> 
+    <Navbar/>
+    <section id='articles'>
+      {news.map((article)=>{
+        return(
+          <Article
+            key={article.id}
+            title={article.title}
+            provider={article.newsSite} 
+            description={article.summary}
+            thumbnail={article.imageUrl}
+          />
+        );  
+      })}
+    </section>
+    </>
+  );
 }
 export default App;
